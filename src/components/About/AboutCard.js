08707d96802bddef8d9309fb78770048b67bdce5
小小星球杯 <nini_good@outlook.com>
2024-06-06 22:07:27 +0800

    init

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Freya Liu </span>
+            from <span className="purple"> Hangzhou, China.</span>
+            <br />
+            I am a Smart Contract Developer with substantial experience in blockchain technology.
+            <br />
+            Additionally, I possess three years of expertise in machine learning and macOS development, complementing my blockchain skills with advanced technological acumen
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Writing Tech Blogs
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Freya</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
